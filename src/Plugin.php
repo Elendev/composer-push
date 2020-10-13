5e85be7b60438b5d126a85ea6ae7c0bd724ffcb7
aspilicious <bramgoffings@gmail.com>
2020-10-13 14:48:36 +0200

    Update Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -20,4 +20,12 @@ class Plugin implements PluginInterface, Capable
           'Composer\Plugin\Capability\CommandProvider' => 'Elendev\NexusComposerPush\PushCommandProvider',
         );
     }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
 }
