c7ce7457f0002e4706dbe8ea2f6f4ca014255442
Jonas Renaudot <jonas.renaudot@swissquote.ch>
2021-07-14 15:01:00 +0200

    Create a "Configuration" class to handle plugin's configuration.

---
+++ src/Configuration.php
@@ -0,0 +1,217 @@
+<?php
+
+
+namespace Elendev\NexusComposerPush;
+
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Symfony\Component\Console\Input\InputInterface;
+
+class Configuration
+{
+    /**
+     * @var array config cache
+     */
+    private $config;
+
+    /**
+     * @var InputInterface
+     */
+    private $input;
+
+    /**
+     * @var Composer
+     */
+    private $composer;
+
+    /**
+     * @var IOInterface
+     */
+    private $io;
+
+    const REPOSITORY = 'repository';
+    const PUSH_CFG_NAME = 'name';
+
+    public function __construct(InputInterface $input, Composer $composer, IOInterface $io) {
+        $this->input = $input;
+        $this->composer = $composer;
+        $this->io = $io;
+        $this->config = $this->parseNexusExtra($input, $composer);
+    }
+
+    /**
+     * Get the Nexus extra values if available
+     * @param $parameter
+     * @param null $default
+     * @return mixed|null
+     */
+    public function get($parameter, $default = null) {
+        if (!empty($this->config[$parameter])) {
+            return $this->config[$parameter];
+        } else {
+            return $default;
+        }
+    }
+
+    /**
+     * Return the package name based on the given name or the real package name.
+     *
+     * @param \Symfony\Component\Console\Input\InputInterface|null $input
+     *
+     * @return string
+     */
+    public function getPackageName() {
+        if ($this->input && $this->input->getOption('name')) {
+            return $this->input->getOption('name');
+        } else {
+            return $this->composer->getPackage()->getName();
+        }
+    }
+
+    /**
+     * Fetch any directories or files to be excluded from zip creation
+     *
+     * @return array
+     */
+    public function getIgnores()
+    {
+        // Remove after removal of --ignore-dirs option
+        $deprecatedIgnores = $this->getDirectoriesToIgnore($this->input);
+
+        $optionalIgnore = $this->input->getOption('ignore');
+        $composerIgnores = $this->get('ignore', []);
+        $gitAttrIgnores = $this->getGitAttributesExportIgnores($this->input);
+        $composerJsonIgnores = $this->getComposerJsonArchiveExcludeIgnores($this->input);
+
+        if (! $this->input->getOption('keep-vendor')) {
+            $defaultIgnores = ['vendor/'];
+        } else {
+            $defaultIgnores = [];
+        }
+
+        $ignore = array_merge($deprecatedIgnores, $composerIgnores, $optionalIgnore, $gitAttrIgnores, $composerJsonIgnores, $defaultIgnores);
+        return array_unique($ignore);
+    }
+
+    /**
+     * @param InputInterface $input
+     * @deprecated argument has been changed to ignore
+     * @return array
+     */
+    private function getDirectoriesToIgnore(InputInterface $input)
+    {
+        $optionalIgnore = $input->getOption('ignore-dirs');
+        $composerIgnores = $this->get('ignore-dirs', []);
+
+        if (!empty($optionalIgnore)) {
+            $this->io->write('<error>The --ignore-dirs option has been deprecated. Please use --ignore instead</error>');
+        }
+
+        if (!empty($composerIgnores)) {
+            $this->io->write('<error>The ignore-dirs config option has been deprecated. Please use ignore instead</error>');
+        }
+
+        $ignore = array_merge($composerIgnores, $optionalIgnore);
+        return array_unique($ignore);
+    }
+
+    private function getGitAttributesExportIgnores(InputInterface $input)
+    {
+        $option = $input->getOption('ignore-by-git-attributes');
+        $extra = $this->get('ignore-by-git-attributes', false);
+        if (!$option && !$extra) {
+            return [];
+        }
+
+        $path = getcwd() . '/.gitattributes';
+        if (!is_file($path)) {
+            return [];
+        }
+
+        $contents = file_get_contents($path);
+        $lines = explode(PHP_EOL, $contents);
+        $ignores = [];
+        foreach ($lines as $line) {
+            if ($line = trim($line)) {
+                // ignore if end with `export-ignore`
+                $diff = strlen($line) - 13;
+                if ($diff > 0 && strpos($line, 'export-ignore', $diff) !== false) {
+                    $ignores[] = trim(trim(explode(' ', $line)[0]), DIRECTORY_SEPARATOR);
+                }
+            }
+        }
+
+        return $ignores;
+    }
+
+    private function getComposerJsonArchiveExcludeIgnores(InputInterface $input)
+    {
+        $option = $input->getOption('ignore-by-composer');
+        $extra = $this->get('ignore-by-composer', false);
+        if (!$option && !$extra) {
+            return [];
+        }
+
+        $path = getcwd() . '/composer.json';
+
+        $contents = file_get_contents($path);
+        $jsonContents = json_decode($contents, true);
+        $ignores = [];
+        if (array_key_exists('archive', $jsonContents) && array_key_exists('exclude', $jsonContents['archive'])) {
+            foreach ($jsonContents['archive']['exclude'] as $exclude) {
+                $ignores[] = trim($exclude, DIRECTORY_SEPARATOR);
+            }
+        }
+
+        return $ignores;
+    }
+
+    /**
+     * @param InputInterface $input
+     */
+    private function parseNexusExtra(InputInterface $input, Composer $composer)
+    {
+        $this->checkNexusPushValid($input, $composer);
+
+        $repository = $input->getOption(self::REPOSITORY);
+        $extras = $composer->getPackage()->getExtra();
+        if (empty($repository)) {
+            // configurations in composer.json support Only upload to unique repository
+            if (!empty($extras['nexus-push'])) {
+                $this->nexusPushConfig = $extras['nexus-push'];
+            }
+        } else {
+            // configurations in composer.json support upload to multi repository
+            foreach ($extras['nexus-push'] as $key=> $nexusPushConfigItem) {
+                if (empty($nexusPushConfigItem[self::PUSH_CFG_NAME])) {
+                    $fmt = 'The nexus-push configuration array in composer.json with index {%s} need provide value for key "%s"';
+                    $exceptionMsg = sprintf($fmt, $key, self::PUSH_CFG_NAME);
+                    throw new InvalidConfigException($exceptionMsg);
+                }
+                if ($nexusPushConfigItem[self::PUSH_CFG_NAME] ==$repository) {
+                    $this->nexusPushConfig = $nexusPushConfigItem;
+                }
+            }
+
+            if (empty($this->nexusPushConfig)) {
+                throw new \InvalidArgumentException('The value of option --repository match no nexus-push configuration, please check');
+            }
+        }
+
+        return $this->nexusPushConfig;
+    }
+
+    private function checkNexusPushValid(InputInterface $input, Composer $composer)
+    {
+        $repository = $input->getOption(self::REPOSITORY);
+        $extras = $composer->getPackage()->getExtra();
+        if (empty($repository) && !empty($extras['nexus-push'][0])) {
+            throw new \InvalidArgumentException('As configurations in composer.json support upload to multi repository, the option --repository is required');
+        }
+        if (!empty($repository) && empty($extras['nexus-push'][0])) {
+            throw new InvalidConfigException('the option --repository is offered, but configurations in composer.json doesn\'t support upload to multi repository, please check');
+        }
+    }
+
+}
