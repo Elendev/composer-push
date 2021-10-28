5efd87b779a40fd42fcd4e01c918d461edbe21d3
LeJeanbono <jean.michel.lec@gmail.com>
2021-10-28 15:07:34 +0200

    Init config before check version

--- src/PushCommand.php
+++ src/PushCommand.php
@@ -98,15 +98,15 @@ EOT
             }
         }
 
+        $fileName = tempnam(sys_get_temp_dir(), 'composer-push') . '.zip';
+
         $composer = $this->getComposer(true);
+
+        $this->configuration = new Configuration($input, $composer, $this->getIO());
 
         if (empty($this->configuration->getVersion())) {
             throw new InvalidArgumentException('No version found, you chould either provide version argument in the command or add version in composer.json');
         }
-
-        $fileName = tempnam(sys_get_temp_dir(), 'composer-push') . '.zip';
-
-        $this->configuration = new Configuration($input, $composer, $this->getIO());
 
         $packageName = $this->configuration->getPackageName();
 
